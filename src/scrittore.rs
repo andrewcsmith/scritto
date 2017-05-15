1a33b38e3878d2c263be488d38a3d2ee79d7f956
Andrew Smith <andrewchristophersmith@gmail.com>
2017-05-15 00:10:34 -0700

    Redo impls of View and Viewable to be nicer

--- src/scrittore.rs
+++ src/scrittore.rs
@@ -21,30 +21,47 @@ where N: Note<D>,
     phantom: PhantomData<D>
 }
 
-pub struct NoteView
+pub struct SingleNoteView<P, D>
 {
     pub data: BTreeMap<String, Value>,
-    hb: Handlebars
+    hb: Handlebars,
+    phantom: PhantomData<(P, D)>
 }
 
-pub struct ChordView
+pub struct ChordView<P, D>
 {
     pub data: BTreeMap<String, Value>,
-    hb: Handlebars
+    hb: Handlebars,
+    phantom: PhantomData<(P, D)>
 }
 
-pub struct NotesView
+pub struct NotesView<N, D>
 {
     pub data: BTreeMap<String, Value>,
-    hb: Handlebars
+    hb: Handlebars,
+    phantom: PhantomData<(N, D)>
 }
 
 /// The fundamental trait for scrittore module. By convention, `format()` instantiates a global
-/// variable as the expected name of the input. That is, a `NoteView` will instantiate its Input
+/// variable as the expected name of the input. That is, a `SingleNoteView` will instantiate its Input
 /// data as the JSON object `note`.
-pub trait View<D: Durational, Input> 
+pub trait View: Sized
 {
-    fn format<'b>(&'b mut self, input: &Input) -> Result<String, &'static str>;
+    type Input;
+
+    fn new(source: String, data: BTreeMap<String, Value>) -> Result<Self, TemplateError>;
+
+    fn render<'b>(&'b mut self, input: &Self::Input) -> Result<String, &'static str>;
+
+    fn init_handlebars(source: String) -> Result<Handlebars, TemplateError> 
+    {
+        let mut hb = Handlebars::new();
+        // Override the default with a no-escape function
+        let escape_fn = |s: &str| -> String { s.to_string() };
+        hb.register_escape_fn(escape_fn);
+        hb.register_template_string("template", source)?;
+        Ok(hb)
+    }
 }
 
 /// `Viewable` sets up a given context allowing for a single element to be rendered. An object will
@@ -53,11 +70,11 @@ pub trait View<D: Durational, Input>
 pub trait Viewable<'a, D>: Sized 
 where D: 'a + Durational
 {
-    type View: View<D, Self>;
+    type View: View<Input=Self>;
 
     fn render<'b>(&self, view: &'b mut Self::View) -> Result<String, &'static str> 
     {
-        view.format(self)
+        view.render(self)
     }
 }
 
@@ -73,81 +90,69 @@ where N: Note<D>,
     }
 }
 
-impl NoteView
-{
-    pub fn new(source: String, data: BTreeMap<String, Value>) -> Result<Self, TemplateError> 
-    {
-        let view = NoteView {
-            data: data,
-            hb: Self::init_handlebars(source)?
-        };
-
-        Ok(view)
-    }
-
-    fn init_handlebars(source: String) -> Result<Handlebars, TemplateError> 
-    {
-        let mut hb = Handlebars::new();
-        // Override the default with a no-escape function
-        let escape_fn = |s: &str| -> String { s.to_string() };
-        hb.register_escape_fn(escape_fn);
-        hb.register_template_string("template", source)?;
-        Ok(hb)
-    }
-
-    pub fn render(&self) -> Result<String, RenderError> {
-        self.hb.render("template", &self.data)
-    }
-}
-
-impl NotesView
-{
-    pub fn new(source: String, data: BTreeMap<String, Value>) -> Result<Self, TemplateError> {
-        let mut view = NotesView {
-            data: data,
-            hb: Self::init_handlebars(source)?
-        };
-
-        Ok(view)
-    }
-
-    /// Overrides the default Handlebars escape function with a no-op. In the future, perhaps this
-    /// should escape for Lilypond.
-    fn init_handlebars(source: String) -> Result<Handlebars, TemplateError> {
-        let mut hb = Handlebars::new();
-        // Override the default with a no-escape function
-        let escape_fn = |s: &str| -> String { s.to_string() };
-        hb.register_escape_fn(escape_fn);
-        hb.register_template_string("template", source)?;
-        Ok(hb)
-    }
-
-    pub fn render(&self) -> Result<String, RenderError> {
-        self.hb.render("template", &self.data)
-    }
-}
-
-impl<'a, D, N> View<D, N> for NoteView
-where D: 'a + Durational,
-      N: Note<D> + Clone + Serialize,
+impl<'a, P, D> View for SingleNoteView<P, D>
+where D: 'a + Durational + Serialize,
+      P: Pitch + Clone + Serialize,
       for<'de> D: Deserialize<'de>
 {
-    fn format<'b>(&'b mut self, input: &N) -> Result<String, &'static str> {
+    type Input = SingleNote<P, D>;
+
+    fn new(source: String, data: BTreeMap<String, Value>) -> Result<Self, TemplateError> 
+    {
+        let hb: Handlebars = Self::init_handlebars(source)?;
+        let phantom = PhantomData;
+        Ok(SingleNoteView { data, hb, phantom })
+    }
+
+    fn render<'b>(&'b mut self, input: &Self::Input) -> Result<String, &'static str> 
+    {
         let in_val = serde_json::to_value(input).map_err(|e| "Could not parse note into value")?;
+        println!("{}", serde_json::to_string(&in_val).unwrap());
         self.data.insert("note".to_string(), in_val);
-        self.render().map_err(|_| "Could not render")
+        self.hb.render("template", &self.data).map_err(|_| "Could not render")
     }
 }
 
-impl<'a, D, N> View<D, Notes<N, D>> for NotesView
-where D: 'a + Durational,
+impl<'a, P, D> View for ChordView<P, D>
+where D: 'a + Durational + Serialize,
+      P: Pitch + Clone + Serialize,
+      for<'de> D: Deserialize<'de>
+{
+    type Input = Chord<P, D>;
+
+    fn new(source: String, data: BTreeMap<String, Value>) -> Result<Self, TemplateError> 
+    {
+        let hb: Handlebars = Self::init_handlebars(source)?;
+        let phantom = PhantomData;
+        Ok(ChordView { data, hb, phantom })
+    }
+
+    fn render<'b>(&'b mut self, input: &Self::Input) -> Result<String, &'static str> {
+        let in_val = serde_json::to_value(input).map_err(|e| "Could not parse chord into value")?;
+        println!("{}", serde_json::to_string(&in_val).unwrap());
+        self.data.insert("chord".to_string(), in_val);
+        self.hb.render("template", &self.data).map_err(|_| "Could not render")
+    }
+}
+
+impl<'a, D, N> View for NotesView<N, D>
+where D: 'a + Durational + Serialize,
       N: Note<D> + Clone + Serialize,
       for<'de> D: Deserialize<'de>
 {
-    fn format<'b>(&'b mut self, input: &Notes<N, D>) -> Result<String, &'static str> {
+    type Input = Notes<N, D>;
+
+    fn new(source: String, data: BTreeMap<String, Value>) -> Result<Self, TemplateError> {
+        let hb: Handlebars = Self::init_handlebars(source)?;
+        let phantom = PhantomData;
+        Ok(NotesView { data, hb, phantom })
+    }
+
+    fn render<'b>(&'b mut self, input: &Notes<N, D>) -> Result<String, &'static str> {
         let in_val = serde_json::to_value(&input.data).map_err(|e| "Could not parse notes into value")?;
+        println!("{}", serde_json::to_string(&in_val).unwrap());
         self.data.insert("notes".to_string(), in_val);
-        self.render().map_err(|_| "Could not render")
+        self.hb.render("template", &self.data).map_err(|_| "Could not render")
     }
 }
 
@@ -156,7 +161,7 @@ where D: 'a + Durational + Serialize,
       P: Pitch + Clone + Serialize,
       for<'de> D: Deserialize<'de>
 {
-    type View = NoteView;
+    type View = SingleNoteView<P, D>;
 }
 
 impl<'a, P, D> Viewable<'a, D> for Chord<P, D>
@@ -164,7 +169,7 @@ where D: 'a + Durational + Serialize,
       P: Pitch + Clone + Serialize,
       for<'de> D: Deserialize<'de>
 {
-    type View = NoteView;
+    type View = ChordView<P, D>;
 }
 
 impl<'a, D, N> Viewable<'a, D> for Notes<N, D>
@@ -172,7 +177,7 @@ where D: 'a + Durational + Serialize,
       N: Note<D> + Clone + Serialize,
       for<'de> D: Deserialize<'de>
 {
-    type View = NotesView;
+    type View = NotesView<N, D>;
 }
 
 #[cfg(test)]
@@ -195,7 +200,7 @@ mod tests {
     fn test_render_note() {
         let notes = initialize_notes();
         let mut data = BTreeMap::new();
-        let mut view = NoteView::new(
+        let mut view = SingleNoteView::new(
             "{{ note.text }}{{ note.ly_duration}}".to_string(),
             data).unwrap();
 
@@ -207,8 +212,8 @@ mod tests {
     fn test_render_chord() {
         let chord: Chord<ETPitch, RatioDuration> = Chord::new(vec![ETPitch::new(60), ETPitch::new(62)], Duration(RatioDuration(1, 2)));
         let mut data = BTreeMap::new();
-        let mut view = NoteView::new(
-            "<{{#each note.pitches as |pitch| }} {{ pitch.ly }} {{ /each }}>{{ note.ly_duration}}".to_string(),
+        let mut view = ChordView::new(
+            "<{{#each chord.pitches as |pitch| }} {{ pitch.ly }} {{ /each }}>{{ chord.ly_duration}}".to_string(),
             data).unwrap();
 
         let out = chord.render(&mut view).unwrap();
